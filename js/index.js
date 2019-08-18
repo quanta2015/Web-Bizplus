5bde0aee50a581e288da33a846d55923080c06f8
quanta2015 <quanta2015@163.com>
2019-08-18 13:27:51 +0900

    update index

--- js/index.js
+++ js/index.js
@@ -10,20 +10,29 @@ var INDEX = {
 
       INDEX.renderCarousel();
       INDEX.renderCard();
+      INDEX.renderBuss();
       INDEX.renderNews();
     })
   },
 
-  // renderNews: function() {
-  //   promiseTmpl('get','/tmpl/index/news.tmpl','/news',null,NO_MASK,function (r,d) {
-  //     $('.m-news').append($.templates(r).render(d, rdHelper));
-  //   });
-  // },
+  renderNews: function() {
+    renderTmpl('/tmpl/index/news.tmpl', function (r) {
+      data = { "news": _langDB[_langDB.cur]['home_news'] }
+      $('.m-news').append($.templates(r).render(data, rdHelper));
+    });
+  },
 
   renderCard: function() {
     renderTmpl('/tmpl/index/cards.tmpl',function (r) {
       data = { "cards": _langDB[_langDB.cur]['home_card'] }
       $('.m-cards').append($.templates(r).render(data, rdHelper));
+    });
+  },
+
+  renderBuss: function() {
+    renderTmpl('/tmpl/index/business.tmpl',function (r) {
+      data = _langDB[_langDB.cur].business
+      $('.m-buss').append($.templates(r).render(data, rdHelper));
     });
   },
 
